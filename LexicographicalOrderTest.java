431ec2f6f94824d5d9698a599b337f29a90d1639
Alexander <keldor@mail.ru>
2016-11-06 20:54:45 +0500

    4 tests
    with the last changes

---
+++ LexicographicalOrderTest.java
@@ -0,0 +1,33 @@
+package equpalt;
+
+import equpalt.ISearcher;
+import equpalt.TrieSearcher;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LexicographicalOrderTest {
+    private ISearcher searcher;
+
+    @Before
+    public void init() {
+        searcher = new TrieSearcher();
+        searcher.refresh(new String[] {"ABC", "ABB", "AAA", "ABA"}, new long[] {0, 0, 0, 0});
+    }
+
+    @Test
+    public void testA() {
+        String[] names = searcher.guess("A");
+        assertTrue(names[0] == "AAA");
+        assertTrue(names[1] == "ABA");
+        assertTrue(names[2] == "ABB");
+        assertTrue(names[3] == "ABC");
+    }
+
+    @Test
+    public void testAA() {
+        String[] names = searcher.guess("AA");
+        assertTrue(names[0] == "AAA");
+    }
+}
